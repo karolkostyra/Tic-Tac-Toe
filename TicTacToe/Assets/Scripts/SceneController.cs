4a4a1627fe0b1a0ebdf78c1506f64328476ca02c
Karol Kostyra <karol.m.kostyra@gmail.com>
2020-03-22 16:23:20 +0100

    New button animation + scene management
    - the old animation was replaced by a new one without a bug causing stops animation in highlight state
    - implemented a new script to managing scenes

---
+++ TicTacToe/Assets/Scripts/SceneController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void testy2()
+    {
+        StartCoroutine(testy());
+    }
+
+    IEnumerator testy()
+    {
+        Debug.Log("waiting...");
+        yield return new WaitForSeconds(.3f);
+        LoadMenu();
+    }
+}
